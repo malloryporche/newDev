909ce17106b1844eec749ae041195343eb2dbf23
malloryporhce <malloryburke@gmail.com>
2018-03-05 14:16:21 -0800

    Added navigation

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 
+app.use(express.static('./public/'));
+
+app.set('view engine', 'pug');
 
 app.get('/', function(req,res) {
-  res.send('Hello World!'); 
+  res.render('index'); 
 });
 
 app.listen(3000, () =>  console.log('App is listening on port 3000!'));
