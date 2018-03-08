a1ba2a691b050b55404030587c9bdd55a87b3e91
malloryporhce <malloryburke@gmail.com>
2018-03-08 12:31:34 -0800

    Added header, fixed templating

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 const app = express();
 
-app.use(express.static('./public/'));
+//  Connection URL
+const url = 'mongodb://localhost:27017';
 
-app.set('view engine', 'pug');
+// Database name
+const dbName = 'recipebook';
 
-app.get('/', function(req,res) {
-  res.render('home'); 
+// Use connect method to connect to the Mongod server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log('Connected successfully to the server');
+
+  const db = client.db(dbName);
+
+  insertRecipes(db, function() {
+      client.close();
+  });
 });
 
-app.listen(3000, () =>  console.log('App is listening on port 3000!'));
+const insertRecipes = function(db, callback) {
+  // Get the recipes collection
+  const recipeCollection = db.collection('recipes');
+  // Insert some recipes
+  recipeCollection.insertMany([
+    {a: 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log('Inserted 3 recipes into the recipe collection');
+    callback(result);
+  });
+};
+
+
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+app.use(express.static('./public/'));
+
+//  Set templating and view engine
+app.set('view engine', 'pug');
+
+const mainRoutes = require('./routes');
+const recipeRoutes = require('./routes/recipes');
+
+app.use(mainRoutes);
+// app.use('/recipes', recipeRoutes);
+
+app.listen(3000, () =>  console.log('Server is running on localhost 3000!'));
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,55 @@
 const express = require('express');
 const router = express.Router();
 
+const recipes = [
+  {
+    'name': 'raw Tacos',
+    'description': 'greatest raw tacos ever',
+    'onMenu': false,
+  },
+  {
+    'name': 'vegan Empanadas',
+    'description': 'greatest empanadas ever',
+    'onMenu': false,
+  },
+  {
+    'name': 'raw desserts',
+    'description': 'greatest raw desserts ever',
+    'onMenu': false,
+  },
+  {
+    'name': 'raw snacks',
+    'description': 'greatest raw snacks ever',
+    'onMenu': false,
+  }
+];
+
 router.get('/', (req, res) => {
-  res.render('home');
+  const name = req.cookies.username;
+  if (name) {
+    res.render('home', {name});
+  } else {
+    res.redirect('/hello');
+  }
 });
+
+router.get('/hello', (req, res) => {
+  const name = req.cookies.username;
+  if (name) {
+    res.render('home', {name});
+  } else {
+    res.render('hello');
+  }
+});
+
+router.post('/hello', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/');
+});
+
+router.get('/recipes', (req, res) => {
+  res.render('recipe', {recipes});
+});
+
+
+module.exports = router;
