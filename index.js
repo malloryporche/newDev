7772355fe36e8bd342b4c8067fa4877ec475945f
malloryporhce <malloryburke@gmail.com>
2018-03-10 11:51:07 -0800

    Added register/login route and signup form

--- index.js
+++ index.js
@@ -3,43 +3,53 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const mongoose = require('mongoose');
 
+//  Start express app
 const app = express();
 
-//  Connection URL
-const url = 'mongodb://localhost:27017';
+//  mongodb Connection
+mongoose.connect('mongodb://localhost:27017/recipebook');
+var db = mongoose.connection;
 
-// Database name
-const dbName = 'recipebook';
-
-// Use connect method to connect to the Mongod server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log('Connected successfully to the server');
-
-  const db = client.db(dbName);
-
-  insertRecipes(db, function() {
-      client.close();
-  });
-});
-
-const insertRecipes = function(db, callback) {
-  // Get the recipes collection
-  const recipeCollection = db.collection('recipes');
-  // Insert some recipes
-  recipeCollection.insertMany([
-    {a: 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log('Inserted 3 recipes into the recipe collection');
-    callback(result);
-  });
-};
-
-
+//  mongo Error
+db.on('error', console.error.bind(console, 'connection error:'));
+//
+// //  Connection URL
+// const url = 'mongodb://localhost:27017';
+//
+// // Database name
+// const dbName = 'recipebook';
+//
+// // Use connect method to connect to the Mongod server
+// MongoClient.connect(url, function(err, client) {
+//   assert.equal(null, err);
+//   console.log('Connected successfully to the server');
+//
+//   const db = client.db(dbName);
+//
+//   insertRecipes(db, function() {
+//       client.close();
+//   });
+// });
+//
+// const insertRecipes = function(db, callback) {
+//   // Get the recipes collection
+//   const recipeCollection = db.collection('recipes');
+//
+//   // Insert some recipes
+//   recipeCollection.insertMany([
+//     {a: 1}, {a : 2}, {a : 3}
+//   ], function(err, result) {
+//     assert.equal(err, null);
+//     assert.equal(3, result.result.n);
+//     assert.equal(3, result.ops.length);
+//     console.log('Inserted 3 recipes into the recipe collection');
+//     callback(result);
+//   });
+//
+//
+// };
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,48 @@ const router = express.Router();
 
 const recipes = [
   {
+    'id': 1,
     'name': 'raw Tacos',
     'description': 'greatest raw tacos ever',
+    'ingredients': [
+      'walnuts',
+      'tomatoes',
+      'jicama shells'
+    ],
     'onMenu': false,
   },
   {
+    'id': 2,
     'name': 'vegan Empanadas',
     'description': 'greatest empanadas ever',
+    'ingredients': [
+      'black lentils',
+      'quinoa',
+      'seasonings'
+    ],
     'onMenu': false,
   },
   {
-    'name': 'raw desserts',
+    'id': 3,
+    'name': 'Raw Desserts',
     'description': 'greatest raw desserts ever',
+    'ingredients': [
+      'Dates',
+      'MozSOrella',
+      'Almonds',
+      'cashews'
+    ],
     'onMenu': false,
   },
   {
-    'name': 'raw snacks',
+    'id': 4,
+    'name': 'Raw Snacks',
     'description': 'greatest raw snacks ever',
     'onMenu': false,
   }
 ];
 
+//  GET /
 router.get('/', (req, res) => {
   const name = req.cookies.username;
   if (name) {
@@ -33,6 +54,7 @@ router.get('/', (req, res) => {
   }
 });
 
+//  GET /hello
 router.get('/hello', (req, res) => {
   const name = req.cookies.username;
   if (name) {
@@ -51,5 +73,22 @@ router.get('/recipes', (req, res) => {
   res.render('recipe', {recipes});
 });
 
+router.get('/recipes/#{_id}', (req, res) => {
+  res.render('recipeHome', {recipes});
+});
+
+//  GET /menu
+router.get('/menu', (req, res, next) => {
+  res.render('menu', { title: 'Our menu', recipes });
+});
+
+router.get('/register', (req, res, next) => {
+  res.render('register', { title: 'Sign up'});
+});
+
+router.get('/goodbye', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/hello');
+});
 
 module.exports = router;
